ea0a1782acac22fb73975e94dfa347960ed6f000
joelthomasmario <joelthomasmario@gmail.com>
2026-07-21 09:51:31 +0530

    Add student list and print names with marks

---
+++ Task6.js
@@ -0,0 +1,10 @@
+let students = [
+    { name: "Joel", marks: 85 },
+    { name: "John", marks: 72 },
+    { name: "David", marks: 90 },
+    { name: "Mary", marks: 65 }
+];
+
+for (let student of students) {
+    console.log(student.name, student.marks);
+}
