2f5486966232e4817c4518b754c345facac072a8
joelthomasmario <joelthomasmario@gmail.com>
2026-07-21 09:53:23 +0530

    Add student object and log properties

---
+++ Task10.js
@@ -0,0 +1,13 @@
+let student = {
+    name: "Joel",
+    age: 20,
+    course: "B.Tech AI & ML",
+    marks: 90,
+    city: "Bangalore"
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
+console.log(student.marks);
+console.log(student.city);
